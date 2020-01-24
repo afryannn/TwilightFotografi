831434a5da0cbff6aa2d004117235495b9dcf1c5
ilyas afriyan <you@example.com>
2020-01-24 11:12:44 +0700

    add jenis camera

---
+++ app/src/main/java/com/afriyan_fr/twilight/jeniscamera.java
@@ -0,0 +1,13 @@
+package com.afriyan_fr.twilight;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class jeniscamera extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_jeniscamera);
+    }
+}
